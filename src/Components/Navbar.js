e43ba8456bec544bf72afd6c3b26625d0d943457
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-17 18:47:49 +0530

    added navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "../Styles/Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="nav nav-bg">
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
