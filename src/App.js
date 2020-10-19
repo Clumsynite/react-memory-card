066988843f648c19af0c6c8f686b12dbc2e0a4d0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-19 18:38:54 +0530

    imported card componnt to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Cards from "./Components/Cards";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Cards />
     </div>
   );
 }
