bf935f2cb7d45698d4df20fcccd0442b9c7f62d4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 14:35:39 +0530

    updated to set cards array on render

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Cards = () => {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    setCards(cardsToArray);
+  }, [cards]);
+  const cardsToArray = () => {
+    const array = [];
+    for (let i = 0; i < 3; i++) {
+      array.push({
+        name: `avenger-${i + 1}`,
+        src: function () {
+          return `../images/${this.name}.jpg`;
+        },
+        clicked: false,
+      });
+    }
+    return array;
+  };
 
-  return(
+  return (
     <div className="Cards">
       Cards
+      {cards.length > 1 && cards[0].src()}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
