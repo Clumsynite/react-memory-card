64b52edf9e4ec6ed5743f061181ed7de8d98db8a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 15:23:08 +0530

    removed cardArray method uses import instead

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from "react";
+import images from './images' 
 
 const Cards = () => {
-  const [cards, setCards] = useState([]);
-  useEffect(() => {
-    setCards(cardsToArray);
-  }, [cards]);
-  const cardsToArray = () => {
-    const array = [];
-    for (let i = 0; i < 3; i++) {
-      array.push({
-        name: `avenger-${i + 1}`,
-        src: function () {
-          return `../images/${this.name}.jpg`;
-        },
-        clicked: false,
-      });
-    }
-    return array;
-  };
+  const [cards, setCards] = useState(images);
+
+  const cardsMap = cards.map((card, index) => {
+    return (
+      <div key={index} className="card" data-index={index}>
+        <img src={card.src} alt={card.name} />
+        <div className="card-body">
+          <div className="card-title">{card.name}</div>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="Cards">
-      Cards
-      {cards.length > 1 && cards[0].src()}
+      {cards.length > 1 && cardsMap}
+      <img src="../images/nav-bg.jpg" alt="AAA" ></img>
     </div>
   );
 };
