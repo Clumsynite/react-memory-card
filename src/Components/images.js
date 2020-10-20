6d9cddf496a868fdb795b3723112e3e6691d4eff
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 17:18:33 +0530

    imported images and updated export array

--- src/Components/images.js
+++ src/Components/images.js
@@ -1,11 +1,17 @@
-import ironman from '../images/ironman.jpg'
+import blackwidow from '../images/blackwidow.jpg'
 import captain from '../images/captain.jpg'
+import gamora from '../images/gamora.jpg'
+import hawkeye from '../images/hawkeye.jpg'
 import hulk from '../images/hulk.jpg'
+import ironman from '../images/ironman.jpg'
+import loki from '../images/loki.jpg'
+import spiderman from '../images/spiderman.jpg'
+import thor from '../images/thor.jpg'
 
 export default [
   {
-    name: `Iron Man`,
-    src: ironman,
+    name: `Black Widow`,
+    src: blackwidow,
     clicked: false,
   },
   {
@@ -14,8 +20,38 @@ export default [
     clicked: false,
   },
   {
+    name: `Gamora`,
+    src: gamora,
+    clicked: false,
+  },
+  {
+    name: `Hawk Eye`,
+    src: hawkeye,
+    clicked: false,
+  },
+  {
     name: `Hulk`,
     src: hulk,
+    clicked: false,
+  },
+  {
+    name: `Iron Man`,
+    src: ironman,
+    clicked: false,
+  },
+  {
+    name: `Loki`,
+    src: loki,
+    clicked: false,
+  },
+  {
+    name: `Spiderman`,
+    src: spiderman,
+    clicked: false,
+  },
+  {
+    name: `Thor`,
+    src: thor,
     clicked: false,
   }
 ]
