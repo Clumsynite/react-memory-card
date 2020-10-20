1dcd78facbf8ca9c64204845479767135cb6528c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 18:41:26 +0530

    added score and topscore state object
    added increase and reset score method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "./Components/Navbar";
 import Cards from "./Components/Cards";
 
 function App() {
+  const [score, setScore] = useState(0)
+  const [best, setBest] = useState(0)
+
+  const increaseScore = () => {
+    setScore(score + 1)
+  }
+  const resetScore = () => {
+    setBest(score)
+    setScore(0)
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar score={score} best={best}/>
       <div className="container mt-3">
-        <Cards />
+        <Cards increaseScore={increaseScore} resetScore={resetScore}/>
       </div>
     </div>
   );
