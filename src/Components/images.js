681272d5491fed811e94b0b202322828a88ba4ba
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 15:12:14 +0530

    added file to iimport and render images

---
+++ src/Components/images.js
@@ -0,0 +1,21 @@
+import ironman from '../images/ironman.jpg'
+import captain from '../images/captain.jpg'
+import hulk from '../images/hulk.jpg'
+
+export default [
+  {
+    name: `Iron Man`,
+    src: ironman,
+    clicked: false,
+  },
+  {
+    name: `Captain America`,
+    src: captain,
+    clicked: false,
+  },
+  {
+    name: `Hulk`,
+    src: hulk,
+    clicked: false,
+  }
+]
