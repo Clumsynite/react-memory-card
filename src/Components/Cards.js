6d2a200e4b75d7c1454105eafe1e6eb0ec6749a4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-20 18:41:44 +0530

    added click handling for each card

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 import images from "./images";
 
-const Cards = () => {
+const Cards = (props) => {
   const [cards, setCards] = useState(images);
 
+  const handleClick = (e) => {
+    const index = e.target.getAttribute('data-index')
+    const clicked = images[index].clicked
+    if(!clicked){
+      props.increaseScore()
+      images[index].clicked = true
+    }else{
+      props.resetScore()
+      images.forEach(image => image.clicked = false)
+    }
+  }
   const cardsMap = cards.map((card, index) => {
     return (
-      <div key={index} className="col mb-3" data-index={index}>
+      <div key={index} className="col mb-3" data-index={index} onClick={handleClick}>
         <div className="card">
           <img src={card.src} alt={card.name} data-index={index} style={{height: "240px"}}/>
           <div className="card-footer font-weight-bold text-center">
