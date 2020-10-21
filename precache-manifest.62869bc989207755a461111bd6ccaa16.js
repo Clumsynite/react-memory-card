9daa4ba544dbb8300bccd330f9d41eef864a0a8c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-21 21:21:29 +0530

    Updates

---
+++ precache-manifest.62869bc989207755a461111bd6ccaa16.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b3d04a5e1318dec216093809b431bd",
+    "url": "/react-memory-card/index.html"
+  },
+  {
+    "revision": "9e475af103a7f5f98dbf",
+    "url": "/react-memory-card/static/css/main.197a779a.chunk.css"
+  },
+  {
+    "revision": "4cbecdf11111b0a78d07",
+    "url": "/react-memory-card/static/js/2.408ca69b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-memory-card/static/js/2.408ca69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e475af103a7f5f98dbf",
+    "url": "/react-memory-card/static/js/main.9b0750f0.chunk.js"
+  },
+  {
+    "revision": "0872daadfcd76da7a71e",
+    "url": "/react-memory-card/static/js/runtime-main.9913b96b.js"
+  },
+  {
+    "revision": "8cf462cf3eb151bfe0a7d57af318c78e",
+    "url": "/react-memory-card/static/media/blackpanther.8cf462cf.jpg"
+  },
+  {
+    "revision": "9a17ee97f7826521d82e7cdf9b1df6a1",
+    "url": "/react-memory-card/static/media/blackwidow.9a17ee97.jpg"
+  },
+  {
+    "revision": "0f9d4bc099e15b01a9c29931b96fbb60",
+    "url": "/react-memory-card/static/media/captain.0f9d4bc0.jpg"
+  },
+  {
+    "revision": "4182118706df77d693d33d48c385055e",
+    "url": "/react-memory-card/static/media/gamora.41821187.jpg"
+  },
+  {
+    "revision": "d98e8ca937a31b744c38c2565f588173",
+    "url": "/react-memory-card/static/media/hawkeye.d98e8ca9.jpg"
+  },
+  {
+    "revision": "ee124c49ef6eb6e7149b104ae8690d0e",
+    "url": "/react-memory-card/static/media/hulk.ee124c49.jpg"
+  },
+  {
+    "revision": "1bbbb6a7fc8376f939965176207d5d4f",
+    "url": "/react-memory-card/static/media/ironman.1bbbb6a7.jpg"
+  },
+  {
+    "revision": "5d30d15a9c14e8adf9986bbbed919fc4",
+    "url": "/react-memory-card/static/media/loki.5d30d15a.jpg"
+  },
+  {
+    "revision": "70fb347bbb69b4b0d31a8e22865caee2",
+    "url": "/react-memory-card/static/media/nav-bg.70fb347b.jpg"
+  },
+  {
+    "revision": "e5438ff91eae422e2e9f7c2cd924fe08",
+    "url": "/react-memory-card/static/media/nickfury.e5438ff9.jpg"
+  },
+  {
+    "revision": "8c08d0221d41bc5fb1467d5fd120b119",
+    "url": "/react-memory-card/static/media/quicksilver.8c08d022.jpg"
+  },
+  {
+    "revision": "9726c950ec6301d23d2f788d45eebe62",
+    "url": "/react-memory-card/static/media/spiderman.9726c950.jpg"
+  },
+  {
+    "revision": "ddd85793843b23358512203b86195792",
+    "url": "/react-memory-card/static/media/starlord.ddd85793.jpg"
+  },
+  {
+    "revision": "f260d63ac25d01e0c48b967a167a84ee",
+    "url": "/react-memory-card/static/media/thor.f260d63a.jpg"
+  },
+  {
+    "revision": "7dbb0bb08741ced0ac189c78336ffe88",
+    "url": "/react-memory-card/static/media/vision.7dbb0bb0.jpg"
+  }
+]);
