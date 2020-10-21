8f442526c61c466c5dd7f008764a8b8d92880467
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-21 14:12:58 +0530

    added scoreboard

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "../Styles/Navbar.css";
-const Navbar = () => {
-  return <div className="Navbar nav nav-bg"></div>;
+const Navbar = (props) => {
+  return (
+    <div className="Navbar">
+      <div className="nav nav-bg"></div>
+      <div className="scoreboard">
+        <div className="current"> Score: {props.score}</div>
+        <div className="best"> Best Score: {props.best}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
