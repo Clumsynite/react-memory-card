1bef7224b9acef2d9af4fb0d978ca13ed62c6bd9
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-21 16:24:24 +0530

    Added out of counter

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../Styles/Navbar.css";
+import images from './images'
 const Navbar = (props) => {
   return (
     <div className="Navbar">
       <div className="nav nav-bg"></div>
       <div className="scoreboard">
-        <div className="current"> Score: {props.score}</div>
+        <div className="current"> Score: {props.score} out of {images.length}</div>
         <div className="best"> Best Score: {props.best}</div>
       </div>
     </div>
